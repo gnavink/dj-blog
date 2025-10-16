36f2ec4144e5ef663a9fb766789797f6f7cf999a
Navin Kumar <gnavink75@gmail.com>
2025-10-16 06:59:33 +0530

    disabled custom blogadmin code

--- blog/admin.py
+++ blog/admin.py
@@ -3,13 +3,15 @@ from django.contrib import admin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin):
+# class PostAdmin(admin.ModelAdmin):
 
-    list_display = [
-        "title",
-        "author",
-        "body",
-    ]
+#     list_display = [
+#         "title",
+#         "author",
+#         "body",
+#     ]
 
 
-admin.site.register(Post, PostAdmin)
+# admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
+
